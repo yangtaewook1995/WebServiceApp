7e7fbe06b33f959164502025d872b74467e4dcb4
Wook <wooke1995@gmail.com>
2021-08-12 13:53:21 +0900

    button & click

---
+++ src/public/main.js
@@ -0,0 +1,6 @@
+const frogBtn = document.getElementById("btn-frog");
+const frogTxt = document.getElementById("txt-frog");
+
+frogBtn.addEventListener('click', function(){
+    frogTxt.innerHTML = "FrogFrog";
+});
